549f7a8331e7dbb51814931c90f6ffe55a7ac8b7
Kyle Reidy <kyle_reidy@prodigy.net>
2020-06-22 22:31:44 -0700

    highCastleHardwareCtrl plugin setup

--- Source/ThirdParty/depshighCastleHardwareCtrl/depshighCastleHardwareCtrl.Build.cs
+++ Source/ThirdParty/depshighCastleHardwareCtrl/depshighCastleHardwareCtrl.Build.cs
@@ -5,9 +5,9 @@ using UnrealBuildTool;
 using System;
 using System.Collections.Generic;
 
-public class deps<#Module Name#> : ModuleRules
+public class depshighCastleHardwareCtrl : ModuleRules
 {
-	public deps<#Module Name#>(ReadOnlyTargetRules Target) : base(Target)
+	public depshighCastleHardwareCtrl(ReadOnlyTargetRules Target) : base(Target)
 	{
 		Type = ModuleType.External;
 
